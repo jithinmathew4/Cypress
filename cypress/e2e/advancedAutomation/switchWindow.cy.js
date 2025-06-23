6eebea133a044793f38ba02976e9fb22aac84406
Jithin Mathew <your-email@example.com>
2025-06-23 20:56:40 +0530

    Add cypress script for swith window

---
+++ cypress/e2e/advancedAutomation/switchWindow.cy.js
@@ -0,0 +1,17 @@
+// cypress/e2e/switchWindow.cy.js
+
+describe('Handling Switch Window on Rahul Shetty Academy', () => {
+    it('Simulates switching to a new window', () => {
+      // Visit the website
+      cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+  
+      // Remove the target attribute to force the link to open in the same tab
+      cy.get('#openwindow').invoke('removeAttr', 'target').click();
+  
+      // Verify the new page URL
+      cy.url().should('include', 'https://www.qaclickacademy.com/');
+  
+      // Perform actions on the new page (if needed)
+      cy.get('.navbar-brand').should('be.visible');
+    });
+  });
